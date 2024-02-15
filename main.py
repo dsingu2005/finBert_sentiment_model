e95f1e5f58c41a612b68fe4abb5fd5e6e3afd754
dsinguUconn <106391395+dsinguUconn@users.noreply.github.com>
2024-02-14 20:07:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
 import gcsfs
@@ -17,10 +18,10 @@ import shutil
 import pickle
 
 # nltk.download('vader_lexicon')
-with open('finbert_model.pkl', 'rb') as f:
-    model, tokenizer = pickle.load(f)
-# model = AutoModelForSequenceClassification.from_pretrained('ProsusAI/finBERT')
-# tokenizer = AutoTokenizer.from_pretrained('ProsusAI/finBERT')
+#with open('finbert_model.pkl', 'rb') as f:
+#    model, tokenizer = torch.load(f)
+model = AutoModelForSequenceClassification.from_pretrained('ProsusAI/finBERT')
+tokenizer = AutoTokenizer.from_pretrained('ProsusAI/finBERT')
 
 # model = AutoModelForSequenceClassification.from_pretrained('mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis')
 # tokenizer = AutoTokenizer.from_pretrained('mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis')
@@ -37,113 +38,22 @@ def analyze_sentiment(text):
     probabilities = softmax(outputs.logits, dim=1)
     return probabilities[0]
 
-#https://console.cloud.google.com/storage/browser/sentiment-files/sentiment_magnitude?hl=en&project=sentiment-analysis-379200&pageState=(%22StorageObjectListTable%22:(%22f%22:%22%255B%255D%22))&prefix=&forceOnObjectsSortingFiltering=false
-# def process_file(filename):
-#     credentials, _ = default()
-#     fs = gcsfs.GCSFileSystem(project='sentiment-analysis-379200', token=credentials)
-#     # fs = gcsfs.GCSFileSystem(project='sentiment-analysis-379200')
-#     with fs.open('gs://sentiment-files/sentiment_magnitude/keywords.xlsx') as f:
-#         keywords_df = pd.read_excel(f)
-#     with fs.open(f'gs://sentiment-files/sentiment_magnitude/{filename}.xlsx') as f:
-#         ads_df = pd.read_excel(f)
 
-#     # Create a directory in the GCS bucket
-#     fs.mkdirs(f'gs://sentiment-files/scores_magnitude{filename}')
-
-#     for column in ads_df.columns:
-#         counts_df = pd.DataFrame(columns=['Key Word Category', 'Keyword', 'Paragraph', 'Sentiment Score', 'Magnitude'])
-
-#         for index, row in keywords_df.iterrows():
-#             keyword = row['Key Words/Topics']
-#             category = row['Key Word Category']
-
-#             paragraphs = ads_df[column].apply(lambda x: str(x) if keyword.lower() in str(x).lower() else None).dropna()
-
-#             for paragraph in paragraphs:
-#                 chunks = split_text(paragraph, 1024)
-#                 for chunk in chunks:
-#                     probabilities = analyze_sentiment(chunk)
-#                     sentiment_score = (probabilities[1] + (probabilities[2] * 2) + (probabilities[0] * 3)) - 2
-                    
-#                     sentences = sent_tokenize(chunk)
-#                     magnitudes = []
-#                     for sentence in sentences:
-#                         sentence_probabilities = analyze_sentiment(sentence)
-#                         sentence_sentiment_score = (sentence_probabilities[1] + (sentence_probabilities[2] * 2) + (sentence_probabilities[0] * 3)) - 2
-#                         sentence_magnitude = abs(sia.polarity_scores(sentence)['compound'])
-#                         magnitudes.append(sentence_magnitude)
-                    
-#                     total_magnitude = sum(magnitudes)
-                    
-#                     new_row = {'Key Word Category': category, 'Keyword': keyword, 'Paragraph': chunk, 'Sentiment Score': sentiment_score.item(), 'Magnitude': total_magnitude}
-#                     counts_df = pd.concat([counts_df, pd.DataFrame([new_row])], ignore_index=True)
-
-#         counts_df.to_excel(f'gs://sentiment-files/scores_magnitude/{filename}/output_{column}.xlsx', index=False)
+def get_quarter(month):
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    else:
+        return 4
 
 
-# def compile_results(folder_name):
-#     output_files = [f for f in os.listdir(folder_name) if f.startswith('output_FINAL TRANSCRIPT') and f.endswith('.xlsx')]
-
-#     compiled_results_sentiment = pd.DataFrame()
-#     compiled_results_magnitude = pd.DataFrame()
-
-#     for file in output_files:
-#         counts_df = pd.read_excel(os.path.join(folder_name, file))
-
-#         date = file.replace('output_FINAL TRANSCRIPT', '').replace('.xlsx', '')
-
-#         average_scores_sentiment = counts_df.groupby('Key Word Category')['Sentiment Score'].mean().reset_index()
-#         average_scores_sentiment.columns = ['Key Word Category', f'Sentiment Score {date}']
-#         average_scores_magnitude = counts_df.groupby('Key Word Category')['Magnitude'].mean().reset_index()
-#         average_scores_magnitude.columns = ['Key Word Category', f'Magnitude {date}']
-
-
-
-# def compile_results(folder_name):
-#     fs = gcsfs.GCSFileSystem(project='sentiment-analysis-379200')
-#     output_files = fs.ls(f'gs://sentiment-files/scores_magnitude/{folder_name}')
-
-#     compiled_results_sentiment = pd.DataFrame()
-#     compiled_results_magnitude = pd.DataFrame()
-
-#     for file in output_files:
-#         with fs.open(file) as f:
-#             counts_df = pd.read_excel(f)
-
-#         date = file.replace('output_FINAL TRANSCRIPT', '').replace('.xlsx', '')
-
-#         average_scores_sentiment = counts_df.groupby('Key Word Category')['Sentiment Score'].mean().reset_index()
-#         average_scores_sentiment.columns = ['Key Word Category', f'Sentiment Score {date}']
-#         average_scores_magnitude = counts_df.groupby('Key Word Category')['Magnitude'].mean().reset_index()
-#         average_scores_magnitude.columns = ['Key Word Category', f'Magnitude {date}']
-
-#         if compiled_results_sentiment.empty:
-#             compiled_results_sentiment = average_scores_sentiment
-#         else:
-#             compiled_results_sentiment = pd.merge(compiled_results_sentiment, average_scores_sentiment, on='Key Word Category', how='outer')
-
-#         if compiled_results_magnitude.empty:
-#             compiled_results_magnitude = average_scores_magnitude
-#         else:
-#             compiled_results_magnitude = pd.merge(compiled_results_magnitude, average_scores_magnitude, on='Key Word Category', how='outer')
-
-#     compiled_results_sentiment.to_excel(f'gs://sentiment-files/scores_magnitude/{folder_name}/Compiled_results_sentiment.xlsx', index=False)
-#     compiled_results_magnitude.to_excel(f'gs://sentiment-files/scores_magnitude/{folder_name}/Compiled_results_magnitude.xlsx', index=False)
-
-#     visualize_results(compiled_results_sentiment, 'Average Sentiment Scores by Keyword Category', folder_name)
-#     visualize_results(compiled_results_magnitude, 'Average Magnitude Scores by Keyword Category', folder_name)
-    # credentials, _ = default()
-    # fs = gcsfs.GCSFileSystem(project='sentiment-analysis-379200', token=credentials)
-
-    # with fs.open('gs://sentiment-files/sentiment_magnitude/keywords.xlsx') as f:
-        # keywords_df = pd.read_excel(f)
-    # with fs.open(f'gs://sentiment-files/sentiment_magnitude/{filename}.xlsx') as f:
-        # ads_df = pd.read_excel(f)
-    
 def process_file(filename):
 
-    keywords_df = pd.read_excel('sentiment/12_20/keywords.xlsx')
-    xls = pd.ExcelFile(f'sentiment/12_20/{filename}.xlsx')
+    keywords_df = pd.read_excel('12_20/keywords.xlsx')
+    xls = pd.ExcelFile(f'{filename}.xlsx')
 
     for sheet_name in xls.sheet_names:
         ads_df = pd.read_excel(xls, sheet_name=sheet_name)
@@ -153,6 +63,22 @@ def process_file(filename):
             os.makedirs(f'./scores_magnitude/{sheet_name}')
 
         for column in ads_df.columns:
+            # Hacky workaround to get date
+            if isinstance(ads_df[column][0], datetime.datetime):
+                date = ads_df[column][0]
+            else:
+                date = pd.to_datetime(column.removeprefix('FINAL TRANSCRIPT'))
+
+            # stop processing sheet if earlier than Q3 2021
+            quarter = get_quarter(date.month)
+            if date.year <= 2021 and quarter < 3:
+                break
+
+            # get output name and skip column if it already exists (avoid redundant processing)
+            output_filename = f'CC_{sheet_name}_Q{quarter}{date.year}_{date.month}_{date.day}_{date.year}'
+            if os.path.exists(f'./scores_magnitude/{sheet_name}/{output_filename}.xlsx'):
+                continue
+
             counts_df = pd.DataFrame(columns=['Key Word Category', 'Keyword', 'Paragraph', 'Sentiment Score', 'Sentiment Magnitude'])
 
             for index, row in keywords_df.iterrows():
@@ -181,7 +107,8 @@ def process_file(filename):
                         counts_df = pd.concat([counts_df, pd.DataFrame([new_row])], ignore_index=True)
 
             # Save the output file with the date of the earnings call
-            counts_df.to_excel(f'./scores_magnitude/{sheet_name}/output_{column}.xlsx', index=False)
+            counts_df.to_excel(f'./scores_magnitude/{sheet_name}/{output_filename}.xlsx',
+            index=False)
 
 def compile_results():
     base_dir = './scores_magnitude'
@@ -250,9 +177,9 @@ def process_folder(folder_name):
     blobs = bucket.list_blobs(prefix=f'scores_magnitude/{folder_name}/')
     urls = []
     for blob in blobs:
-        if blob.name.endswith('.png'):  # Check if the blob name ends with .png
+        if blob.name.endswith('.png'):
             print(f'Processing file: {blob.name}')
-            url = blob.public_url  # Get the public URL of the blob
+            url = blob.public_url  
             urls.append(url)
     return urls
 
